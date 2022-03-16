3808bdc68678928293b60b13337b59480c06beed
Orcicorn <orcicorn@gmail.com>
2022-03-16 13:39:05 -0400

    Fix DisplayCategory for Books 1-3

---
+++ Contents/mods/FirearmBooksExpanded/media/lua/client/FireBookTweaks.lua
@@ -0,0 +1,14 @@
+require("IteamTweaker_Core");
+
+-- If Firearm Books is enabled, fix the display categories
+if getActivatedMods():contains("FireBook") then
+    TweakItem("FireBook.Firearm1", "DisplayCategory", "SkillBook");
+    TweakItem("FireBook.Firearm2", "DisplayCategory", "SkillBook");
+    TweakItem("FireBook.Firearm3", "DisplayCategory", "SkillBook");
+    TweakItem("FireBook.Reloading1", "DisplayCategory", "SkillBook");
+    TweakItem("FireBook.Reloading2", "DisplayCategory", "SkillBook");
+    TweakItem("FireBook.Reloading3", "DisplayCategory", "SkillBook");
+end
+
+-- Don't have to tweak my own books as their definitions include the 
+-- skillbook category in /media/scripts/FirebookExpanded-Books.txt
