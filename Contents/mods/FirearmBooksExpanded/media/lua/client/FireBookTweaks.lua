7e265738e47903e03751f4348d74318cce3ea864
Orcicorn <orcicorn@gmail.com>
2022-09-09 11:15:34 -0400

    41.73 Compatibility

--- Contents/mods/FirearmBooksExpanded/media/lua/client/FireBookTweaks.lua
+++ Contents/mods/FirearmBooksExpanded/media/lua/client/FireBookTweaks.lua
@@ -1,10 +1,12 @@
 require("ItemTweaker_Core");
-TweakItem("FireBook.Firearm1", "DisplayCategory", "SkillBook");
-TweakItem("FireBook.Firearm2", "DisplayCategory", "SkillBook");
-TweakItem("FireBook.Firearm3", "DisplayCategory", "SkillBook");
-TweakItem("FireBook.Reloading1", "DisplayCategory", "SkillBook");
-TweakItem("FireBook.Reloading2", "DisplayCategory", "SkillBook");
-TweakItem("FireBook.Reloading3", "DisplayCategory", "SkillBook");
+if getActivatedMods():contains("FireBook") then
+    TweakItem("FireBook.Firearm1", "DisplayCategory", "SkillBook");
+    TweakItem("FireBook.Firearm2", "DisplayCategory", "SkillBook");
+    TweakItem("FireBook.Firearm3", "DisplayCategory", "SkillBook");
+    TweakItem("FireBook.Reloading1", "DisplayCategory", "SkillBook");
+    TweakItem("FireBook.Reloading2", "DisplayCategory", "SkillBook");
+    TweakItem("FireBook.Reloading3", "DisplayCategory", "SkillBook");
+end
 
 -- Don't have to tweak my own books as their definitions include the 
 -- skillbook category in /media/scripts/FirebookExpanded-Books.txt
